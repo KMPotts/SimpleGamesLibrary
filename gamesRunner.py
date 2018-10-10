5e9b066f4ba5f32c083fe979579b4681bca3ac2a
kmp672 <kmp672@cs.bham.ac.uk>
2018-10-10 23:23:09 +0100

    Proper version of gameRunner added

--- gamesRunner.py
+++ gamesRunner.py
@@ -1,9 +1,11 @@
 import fizzBuzz
 
+#lets the user pick which game they want to play
 def start():
     game = 0
     print("Which game would you like to play? Type in the number for that game.\n1) FizzBuzz")
     game = input()
+    #runs the file for the chosen game
     if(game == str(1)):
         fizzBuzz.main()
     else:
